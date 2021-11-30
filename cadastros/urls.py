de7cf8f73e02b9aa4b6c5dca39b9aa52401b0857
Rafael Zottesso <rafael.zottesso@ifpr.edu.br>
2021-11-30 08:47:46 -0300

    Telas para inserir, alterar e listar cidades e estados.

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import EstadoCreate, EstadoUpdate
+
+from .views import EstadoCreate, CidadeCreate
+from .views import EstadoUpdate, CidadeUpdate
+from .views import EstadoList, CidadeList
 
 urlpatterns = [
     # Criar todos os endereços/rotas
@@ -7,5 +10,10 @@ urlpatterns = [
 
     path('inserir/estado/', EstadoCreate.as_view(), name='cadastrar-estado'),
     path('editar/estado/<int:pk>/', EstadoUpdate.as_view(), name='editar-estado'),
+    path('listar/estado/', EstadoList.as_view(), name='listar-estado'),
+
+    path('inserir/cidade/', CidadeCreate.as_view(), name='cadastrar-cidade'),
+    path('editar/cidade/<int:pk>/', CidadeUpdate.as_view(), name='editar-cidade'),
+    path('listar/cidade/', CidadeList.as_view(), name='listar-cidade'),
     
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,6 +1,7 @@
 from .models import Cidade, Estado
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 
 from django.urls import reverse_lazy
 
@@ -17,3 +18,32 @@ class EstadoUpdate(UpdateView):
     fields = ['nome']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('index-2')
+
+
+class EstadoList(ListView):
+    model = Estado
+    template_name = 'cadastros/listas/estados.html'
+
+
+#######################################
+
+
+class CidadeCreate(CreateView):
+    model = Cidade
+    fields = ['nome', 'habitantes', 'estado']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index-2')
+
+
+class CidadeUpdate(UpdateView):
+    model = Cidade
+    fields = ['nome', 'habitantes', 'estado']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index-2')
+
+
+class CidadeList(ListView):
+    model = Cidade
+    template_name = 'cadastros/listas/cidades.html'
+
+#######################################
